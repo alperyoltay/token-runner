4f3edb842803c59c8e2a16ef437dfc0cde0d2a48
Alper <118008590+alperyoltay@users.noreply.github.com>
2025-01-07 16:49:06 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const { Discord, Client } = require('discord.js');
+const fs = require('fs');
+const data = fs.readFileSync('./tokens.txt', { encoding: 'utf-8' });
+
+
+Array.prototype.clear = function() {
+    let newArray = [];
+    for (let i of this) {
+		if (!newArray.includes(i) && i.length > 0) newArray.push(i);
+    }
+    return newArray;
+}
+
+function convertArray(tokens) {
+	let converted = tokens.split(/\n/).map(x => x.trim().replace("\r", ""));
+	return converted.clear()
+}
+
+const tokens = convertArray(data);
+
+for (var i = 0; i < tokens.length; i++) {
+  const client = new Client();
+
+  client.on("ready", async () => {
+    setInterval(() => {client.channels.get("891801834820345907").messages.get('891801726519238676').then(msg => {msg.react('👍')
+        }) ,10000})})
+	client.login(tokens[i]).then(x => console.log(`${client.user.tag} logged in.`)).catch(err => console.error(err));
+  setTimeout(() => {
+	client.on('ready', () => {
+		client.destroy()
+	})
+  }, i*500)
+};
+
+
---
+++ run.bat
@@ -0,0 +1,2 @@
+for /l %%x in (1, 1, 100) do node app.js
+pause
